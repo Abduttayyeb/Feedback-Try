346f96fb0fac5706cd456dafa5ce0d11221ddd19
abduttayyeb <absking786@gmail.com>
2023-08-05 15:47:50 +0530

    Unused removal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Header from "./components/Header";
-import FeedbackData from "./data/feedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
